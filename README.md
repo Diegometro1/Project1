51dea7b0b411fc7d4d5050ea764f38b686dac310
Diego Acosta <diegometro1@hotmail.com>
2017-09-10 23:37:30 -0400

    adding new files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Project1
-First Cscie15 Project
+#This is the Project # 1
+
+#LIVE URL OF THE PROJECT
+#http://p1.diegoacosta.cat/Project1/
+
+#DESCRIPTION OF THE PROJECT
+System Setup / Portfolio
+Ensure Git and server workflow is in working order.
+Set up a portfolio to present all the projects.
+
+#MY SCREENCAST DEMO
+ https://www.youtube.com/watch?v=Uu2WFKR5kF4&feature=youtu.be
+ 
+ #ANY DETAILS THE INSTRUCTOR OR TA NEEDS TO KNOW ABOUT USING MY PROJECT.
+0 comments. for this project
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Projects</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+<div id="main">
+    <div>
+	    <h1>DIEGO ACOSTA</h1>
+    </div>
+
+
+    <div id="mypic">
+	    <img src="img/image2.JPG" />
+    </div>
+
+
+    <div>
+
+        <h3>About me</h3>
+        <p>I'm 37 years old, i born in Ecuador, i have been living in boston 15 years.</p>
+        <p>I live in East Boston, is a great place where to live, everything is close , just the rent
+        is too expensive now.</p>
+        <p>English is my second Language , i speak Spaish</p>
+        <p>I like to learn new things , and Web Development is something really cool and
+            fun to learn</p>
+
+    </div>
+
+
+    <div>
+
+        <h1>RANDOM QUOTE</h1>
+
+    </div>
+
+
+
+
+
+</div>
+
+
+</body>
+</html>
